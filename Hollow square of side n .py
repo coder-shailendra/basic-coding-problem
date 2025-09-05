3f9b5b213d32421b42aec5e6f139e8556ee68dbf
coder-shailendra <shailedrakumar25@gmail.com>
2025-09-05 23:02:22 +0530

    solved rectangle problem

---
+++ Hollow square of side n .py
@@ -0,0 +1,10 @@
+for i in range(5):
+    if i==0 or i==4:
+        for j in range(5):
+         print("*",end='')
+    else:
+     for j in range(5):
+         if j==0 or j==4:
+             print("*",end='')
+     else:
+        print()
