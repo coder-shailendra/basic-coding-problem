469c85f7cd1b864098022651e0d114a575dccc9d
coder-shailendra <shailedrakumar25@gmail.com>
2025-09-12 07:41:58 +0530

    solved traffic signal

---
+++ Traffic signal system.py
@@ -0,0 +1,9 @@
+signal=float(input("enter the signal"))
+if      signal==red:
+    print(Stop)
+elif signal==yellow:
+    print("get ready")
+elif signal==green:
+     print(go)
+else:
+     print("invalid signal")
