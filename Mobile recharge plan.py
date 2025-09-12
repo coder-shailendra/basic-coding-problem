c913a3cd5b6c6b7a90009a67a2d310ff04e7c830
coder-shailendra <shailedrakumar25@gmail.com>
2025-09-12 07:41:16 +0530

    solved mobile problem

---
+++ Mobile recharge plan.py
@@ -0,0 +1,9 @@
+price=int(input("enter the price:"))
+if price==199:
+    Print("28 days, 1.5GB/day")
+elif price==399:
+    print("56 days, 2GB/day")
+elif price==599:
+    print("84 days, 3GB/day")
+else:
+    print("invalid plan")
