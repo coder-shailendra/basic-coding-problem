f25b40ba6900bc98314c2ed3dd37dc9b80c33120
coder-shailendra <shailedrakumar25@gmail.com>
2025-09-19 17:24:21 +0530

    solved triangle problem

---
+++ Triangle_type.py
@@ -0,0 +1,9 @@
+a=str(input("enter side a: "))
+b=str(input("enter side b: "))
+c=str(input("enter side c: "))
+if all side are equal:
+    print("equilateral")
+elif "two sides are equal":
+    print ("isosceles")
+else:
+    print("scalene")
