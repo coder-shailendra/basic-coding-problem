3c224d2fc3f220ac07d139cda2a8b3e0bf9287e8
coder-shailendra <shailedrakumar25@gmail.com>
2025-09-21 04:32:23 +0530

    solved exam hall entry

---
+++ Exam_hall_entry.py
@@ -0,0 +1,7 @@
+''' Exam Hall Entry
+Allow only if hall ticket and ID card both are valid.'''
+document=input("enter the document")
+if document=="id card and hall ticket":
+    print("valid")
+else:
+    print("not valid")
